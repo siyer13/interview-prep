33400fe0ae06675def07fc64a0ec9631a93428a9
siyer13 <sreedharriyer@gmail.com>
2018-10-12 09:52:08 +0530

    PRB-2 Merge two sorted arrays

--- leetcode/MergeSortedArrays.java
+++ leetcode/MergeSortedArrays.java
@@ -5,11 +5,34 @@ public class MergeSortedArrays {
         int[] arr3 = new int[arr1.length + arr2.length];
         System.out.print("Array1: [");
         for(int i = 0 ; i < arr1.length; i++) {
-            if(i == arr1.length -1) 
+            if(i == arr1.length -1)
                 System.out.print(arr1[i]);
             else
                 System.out.print(arr1[i] + ",");
         }
         System.out.println("]");
+        System.out.print("Array2: [");
+        for(int i = 0 ; i < arr2.length; i++) {
+            if(i == arr2.length -1)
+                System.out.print(arr2[i]);
+            else
+                System.out.print(arr2[i] + ",");
+        }
+        System.out.println("]");
+        int arr1_size = arr1.length;
+        int arr2_size = arr2.length;
+        arr3 = new MergeSortedArrays().mergeSortedArrays(arr1, arr2, arr1_size, arr2_size);
+        System.out.print("Array3: [");
+        for(int i = 0 ; i < arr3.length; i++) {
+            if(i == arr3.length -1)
+                System.out.print(arr3[i]);
+            else
+                System.out.print(arr3[i] + ",");
+        }
+        System.out.println("]");
+   }
+
+   public int[] mergeSortedArrays(int[] arr1, int[] arr2, int size1, int size2) {
+         return arr1;
    }
 }
