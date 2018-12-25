4a4da598affdcfdcc770a353adc86f9bed722bc8
Sridhar <sreedharriyer@gmail.com>
2018-12-25 22:38:21 +0530

    Create ReplaceNextBiggestInteger.java

---
+++ oracle/ReplaceNextBiggestInteger.java
@@ -0,0 +1,7 @@
+/**
+	 * Replace each number with the next bigger number from right side of current index.
+	 * If no bigger number found, print the no itself.
+	 * E.g.
+	 * input;  2,5,9,6,3,4,8,15,12
+	 * output: 3,6,12,8,4,8,12,15,12
+	 */
